997d22cefe7456be71033c53b9cf1c167e28d6a7
Luizfbm <contatoluizfbm@gmail.com>
2026-05-21 14:52:44 -0300

    feat: serve app assets under app route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
-
+import {Request, Response} from "express"
 const app = express();
 const PORT = 8080;
-
-app.use(express.static("."));
+function handlerReadiness(req : Request, res:Response){
+    res.set({"Content-type": "text/plain"})
+    res.send({"body": "OK"})
+}
+app.get("/healthz", handlerReadiness);
+app.use("/app",express.static("./src/app"));
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
+  
 });
