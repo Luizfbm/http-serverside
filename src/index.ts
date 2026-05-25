50000cf4de37c7ceba59137087de975a5709ec18
Luizfbm <contatoluizfbm@gmail.com>
2026-05-25 11:45:01 -0300

    chore: add tsx dev workflow

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import express from "express";
-import {Request, Response} from "express"
+import {Request, Response, NextFunction} from "express"
 const app = express();
 const PORT = 8080;
-function handlerReadiness(req : Request, res:Response){
+app.use(middlewareLogResponses)
+function handlerReadiness(req : Request, res:Response, next : NextFunction){
     res.set({"Content-type": "text/plain"})
     res.send({"body": "OK"})
 }
-app.get("/healthz", handlerReadiness);
-app.use("/app",express.static("./src/app"));
+app.get("/healthz",handlerReadiness);
+app.use("/app", express.static("./src/app"));
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-  
-});
+function middlewareLogResponses(req: Request,res: Response, next: NextFunction){
+     res.on("finish", () => {
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`)
+    })
+    next()
+  }
+
+app.listen(PORT
+  //, () => {
+  //console.log(`Server is running attheres http://localhost:${PORT}`);
+  //}
+);
